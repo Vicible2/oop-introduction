acd74f4343e4027774726a62f0278844de442749
Manten Devillé <manten.d@hotmail.com>
2021-01-14 13:30:53 +0100

    Add todos for ex 2 and 3

--- 03.Private/exercise_3_private.php
+++ 03.Private/exercise_3_private.php
@@ -3,13 +3,13 @@
 declare(strict_types=1);
 
 /* EXERCISE 3
-Copy the code of excercise 2 to here and delete everything related to cola.
-Make all properties private.
-Make all the other prints work without error.
-After fixing the errors. Change the color of Duvel to light instead of blond and
-also print this new color on the screen after all the other things that were already printed (to be sure that the color has changed).
-Create a new private method in the Beer class called beerInfo which returns "Hi i'm Duvel and have an alochol percentage of 8.5 and I have a light color."
-Make sure that u use the variables and not just this text line.
-Print this method on the screen on a new line.
+TODO:Copy the code of excercise 2 to here and delete everything related to cola.
+TODO:Make all properties private.
+TODO:Make all the other prints work without error.
+TODO:After fixing the errors. Change the color of Duvel to light instead of blond and
+TODO:also print this new color on the screen after all the other things that were already printed (to be sure that the color has changed).
+TODO:Create a new private method in the Beer class called beerInfo which returns "Hi i'm Duvel and have an alochol percentage of 8.5 and I have a light color."
+TODO:Make sure that u use the variables and not just this text line.
+TODO:Print this method on the screen on a new line.
 USE TYPEHINTING EVERYWHERE!
 */
